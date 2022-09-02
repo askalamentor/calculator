3db64bf27c9597b3cdda164ef12e1f3b37cebef2
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-09-02 22:01:00 +0300

    Add keyboard accesskeys

--- script.js
+++ script.js
@@ -14,9 +14,25 @@ let isNumber = false;
 
 // -----------------------------------------------------------------------------------
 // buttons event listeners
-numberButtons.forEach((numberButton) => {
-    numberButton.addEventListener("click", () => {
+window.addEventListener("keydown", handleKeyboardInputs);
 
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener("click", () => addNumber(numberButton.value))
+})
+
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", () => addOperator(operatorButton.value))
+})
+
+equalityButton.addEventListener("click", () => evaluate);
+clearButton.addEventListener("click", clearLastNumber);
+clearAllButton.addEventListener("click", clearAllNumber);
+decimalPointButton.addEventListener("click", addDecimalPoint);
+
+// -----------------------------------------------------------------------------------
+// functions
+function addNumber(number) {
+    
         // check if it is after operator or initial entry
         // check also if it's 0.,, float point
         if (!(currentNumberTab.innerHTML === "") && (currentNumberTab.innerHTML != "0.")) {
@@ -26,83 +42,84 @@ numberButtons.forEach((numberButton) => {
         isNumber = true; // turn on number
 
         // do not let "01, 02..."
-        if (currentNumberTab.innerHTML != "0") {
-            currentNumberTab.innerHTML += numberButton.value;
+        if (currentNumberTab.innerHTML != "0")  {
+
+            currentNumberTab.innerHTML += number;
+
+        // do not let to crash app when press "0" while 0 is at currentNumberTab
+        } else if (currentNumberTab.innerHTML == "0") {
+
+            currentNumberTab.innerHTML = "";
+            currentNumberTab.innerHTML += number;
+
         }
+        
+}
 
-    })
-})
+function addOperator(operator) {
 
-operatorButtons.forEach((operatorButton) => {
-    operatorButton.addEventListener("click", () => {
+    isNumber = false; // turn on operator process     
 
-        isNumber = false; // turn on operator process     
+    // after first calculation
+    if (calculationProcessTabArr.length > 0) {
 
-        // after first calculation
-        if (calculationProcessTabArr.length > 0) {
+        let isLastElementOperator = calculationProcessTabArr.slice(-1);
 
-            let isLastElementOperator = calculationProcessTabArr.slice(-1);
-
-            // prevent to add more than one operator in a row
-            if (checkLastElement(isLastElementOperator) && (currentNumberTab.innerHTML == "")) {
-
-                // edit array
-                calculationProcessTabArr.pop();
-                calculationProcessTabArr.push(operatorButton.value);
-
-                // edit tabs
-                calculationProcessTab.innerHTML = "";
-                calculationProcessTab.innerHTML += calculationProcessTabArr[0] + " " + operatorButton.value + " ";
-                currentNumberTab.innerHTML = "";
-
-
-
-            }
-            // perform usual calculation order
-            else if (checkLastElement(isLastElementOperator) && !(currentNumberTab.innerHTML == "")) {
-
-                performCalculationProcess();
-
-                calculationProcessTabArr.push(operatorButton.value);
-                calculationProcessTab.innerHTML += operatorButton.value + " ";
-
-            }
-            // perform calculation while numbers are available in tabs without operator
-            else if (!checkLastElement(isLastElementOperator) && !(currentNumberTab.innerHTML == "")) {
-
-                // edit array
-                calculationProcessTabArr.push(operatorButton.value);
-
-                performCalculationProcess();
-
-            }
-            // continue to calculation after using equality button
-            else if (!checkLastElement(isLastElementOperator) && (currentNumberTab.innerHTML == "")) {
-
-                calculationProcessTabArr.push(operatorButton.value);
-                calculationProcessTab.innerHTML += operatorButton.value + " ";
-
-            }
-
-            // the first calculation
-        } else {
+        // prevent to add more than one operator in a row
+        if (checkLastElement(isLastElementOperator) && (currentNumberTab.innerHTML == "")) {
 
             // edit array
-            calculationProcessTabArr.push(currentNumberTab.innerHTML);
-            calculationProcessTabArr.push(operatorButton.value);
+            calculationProcessTabArr.pop();
+            calculationProcessTabArr.push(operator);
 
             // edit tabs
-            calculationProcessTab.innerHTML += currentNumberTab.innerHTML + " " + operatorButton.value + " ";
+            calculationProcessTab.innerHTML = "";
+            calculationProcessTab.innerHTML += calculationProcessTabArr[0] + " " + operator + " ";
             currentNumberTab.innerHTML = "";
 
         }
+        // perform usual calculation order
+        else if (checkLastElement(isLastElementOperator) && !(currentNumberTab.innerHTML == "")) {
 
-        console.log(calculationProcessTabArr)
+            performCalculationProcess();
 
-    })
-})
+            calculationProcessTabArr.push(operator);
+            calculationProcessTab.innerHTML += operator + " ";
 
-equalityButton.addEventListener("click", () => {
+        }
+        // perform calculation while numbers are available in tabs without operator
+        else if (!checkLastElement(isLastElementOperator) && !(currentNumberTab.innerHTML == "")) {
+
+            // edit array
+            calculationProcessTabArr.push(operator);
+
+            performCalculationProcess();
+
+        }
+        // continue to calculation after using equality button
+        else if (!checkLastElement(isLastElementOperator) && (currentNumberTab.innerHTML == "")) {
+
+            calculationProcessTabArr.push(operator);
+            calculationProcessTab.innerHTML += operator + " ";
+
+        }
+
+        // the first calculation
+    } else {
+
+        // edit array
+        calculationProcessTabArr.push(currentNumberTab.innerHTML);
+        calculationProcessTabArr.push(operator);
+
+        // edit tabs
+        calculationProcessTab.innerHTML += currentNumberTab.innerHTML + " " + operator + " ";
+        currentNumberTab.innerHTML = "";
+
+    }
+
+}
+
+function evaluate() {
 
     isNumber = false; // turn on operator process 
 
@@ -110,14 +127,8 @@ equalityButton.addEventListener("click", () => {
         performCalculationProcess();
     }
 
-});
+}
 
-clearButton.addEventListener("click", clearLastNumber);
-clearAllButton.addEventListener("click", clearAllNumber);
-decimalPointButton.addEventListener("click", addDecimalPoint);
-
-// -----------------------------------------------------------------------------------
-// functions
 function resetCurrentEntry() {
 
     if (!isNumber) {
@@ -188,8 +199,6 @@ function performCalculationProcess() {
             calculationProcessTabArr.push(deleteTrailingZeros(currentNumberTab.innerHTML));
         }
         
-        console.log(calculationProcessTabArr);
-
     }
 
     // clear currentNumberTab after calculation
@@ -224,6 +233,15 @@ function deleteTrailingZeros(number) {
 
 }
 
-// 0'a bölme
-// tuş ekleme
-// 0 % engelle
+function handleKeyboardInputs(e) {
+
+    if (e.key === "Backspace") clearLastNumber();
+    if (e.key === "Delete" || e.key === "Escape") clearAllNumber();       
+    if(e.key === "," || e.key === ".") addDecimalPoint();
+    if (e.key >= 0 && e.key <= 9) addNumber(e.key);
+    if (e.key === "+" || e.key === "-" || e.key === "*" 
+    || e.key === "/" || (e.key === "%" && e.shiftKey === true) ) addOperator(e.key);
+    if (e.key === "Enter") evaluate();
+
+}
+
